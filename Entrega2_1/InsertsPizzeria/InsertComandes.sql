38dfa56bcd7d40f6891a218a5969887c694cfd6f
PaudelaGC <80599344+PaudelaGC@users.noreply.github.com>
2023-02-22 10:06:33 +0100

    Add files via upload

---
+++ Entrega2_1/InsertsPizzeria/InsertComandes.sql
@@ -0,0 +1,8 @@
+insert into comandes (`Client`, Botiga, `Data/Hora`, Entrega, Preu_total, Repartidor, `Data/hora_Entrega`)
+values ('3', '2', '2022-06-05 18:00:00', '0', '0', Null, Null);
+insert into comandes (`Client`, Botiga, `Data/Hora`, Entrega, Preu_total, Repartidor, `Data/hora_Entrega`)
+values ('1', '2', '2022-06-06 18:00:00', '0', '0', '4', '2022-06-06 18:30:00');
+insert into comandes (`Client`, Botiga, `Data/Hora`, Entrega, Preu_total, Repartidor, `Data/hora_Entrega`)
+values ('2', '2', '2022-06-07 18:00:00', '0', '0', '5', '2022-06-07 18:40:00');
+insert into comandes (`Client`, Botiga, `Data/Hora`, Entrega, Preu_total, Repartidor, `Data/hora_Entrega`)
+values ('1', '4', '2022-06-07 19:00:00', '0', '0', Null, Null);
