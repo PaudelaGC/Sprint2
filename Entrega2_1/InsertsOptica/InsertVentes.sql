4ea3e437261ab622445fd4e291b6388053775332
PaudelaGC <80599344+PaudelaGC@users.noreply.github.com>
2023-02-22 10:09:38 +0100

    Update InsertVentes.sql

--- Entrega2_1/InsertsOptica/InsertVentes.sql
+++ Entrega2_1/InsertsOptica/InsertVentes.sql
@@ -1,14 +1,14 @@
 insert into ventes (Ulleres, Comprador, Venedor, Data_Venta)
-values ('3', '1', '2', '2023-04-04');
+values ('1', '1', '2', '2023-04-04');
 insert into ventes (Ulleres, Comprador, Venedor, Data_Venta)
-values ('3', '2', '4', '2023-04-04');
+values ('1', '2', '4', '2023-04-04');
 insert into ventes (Ulleres, Comprador, Venedor, Data_Venta)
-values ('4', '1', '1', '2023-04-04');
+values ('2', '1', '1', '2023-04-04');
 insert into ventes (Ulleres, Comprador, Venedor, Data_Venta)
-values ('6', '4', '3', '2023-04-04');
+values ('4', '4', '3', '2023-04-04');
 insert into ventes (Ulleres, Comprador, Venedor, Data_Venta)
-values ('5', '3', '1', '2023-04-04');
+values ('3', '3', '1', '2023-04-04');
 insert into ventes (Ulleres, Comprador, Venedor, Data_Venta)
-values ('5', '1', '1', '2023-04-04');
+values ('3', '1', '1', '2023-04-04');
 insert into ventes (Ulleres, Comprador, Venedor, Data_Venta)
-values ('6', '2', '3', '2023-04-04');
+values ('4', '2', '3', '2023-04-04');
